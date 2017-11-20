43119092512de38fe8d97507c66eabecd81fe1d8
tdallas <tdallas@itba.edu.ar>
2017-11-19 23:03:36 -0300

    Notifications

---
+++ app/src/main/java/com/example/tomas/android_app/SelectRoomToDisplay.java
@@ -0,0 +1,23 @@
+package com.example.tomas.android_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by tomas on 19/11/2017.
+ */
+
+public class SelectRoomToDisplay extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.select_room);
+    }
+}
--- app/src/main/java/com/example/tomas/android_app/Settings.java
+++ app/src/main/java/com/example/tomas/android_app/Settings.java
@@ -11,6 +11,5 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
-
     }
 }
