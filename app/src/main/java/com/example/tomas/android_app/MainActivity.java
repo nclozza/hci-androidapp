fb34b4d12b82d98199a93905c38d8bd3d770bb90
nclozza <nclozza@itba.edu.ar>
2017-11-23 01:56:42 -0300

    Cleanup

--- app/src/main/java/com/example/tomas/android_app/MainActivity.java
+++ app/src/main/java/com/example/tomas/android_app/MainActivity.java
@@ -1,11 +1,8 @@
 package com.example.tomas.android_app;
 
-import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.NotificationCompat;
@@ -177,24 +174,16 @@ public class MainActivity extends AppCompatActivity {
 
     public static void sendNotification(String title, String text) {
 
-//Get an instance of NotificationManager//
-
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(context)
                         .setSmallIcon(R.drawable.ic_notifications_black_24dp)
                         .setContentTitle(title)
                         .setContentText(text);
 
-
-// Gets an instance of the NotificationManager service//
-
         NotificationManager mNotificationManager =
-
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
 
-//When you issue multiple notifications about the same type of event, it’s best practice for your app to try to update an existing notification with this new information, rather than immediately creating a new notification. If you want to update this notification at a later date, you need to assign it an ID. You can then use this ID whenever you issue a subsequent notification. If the previous notification is still visible, the system will update this existing notification, rather than create a new one. In this example, the notification’s ID is 001//
-
-                mNotificationManager.notify(001, mBuilder.build());
+        mNotificationManager.notify(001, mBuilder.build());
     }
 }
 
