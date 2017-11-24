ac4cc2fa753d3bbf25cd5a9eeefb0d881073e8dc
nclozza <nclozza@itba.edu.ar>
2017-11-23 22:33:47 -0300

    Changed BASE_URL in singletonAPI

--- app/src/main/java/com/example/tomas/android_app/API/SingletonAPI.java
+++ app/src/main/java/com/example/tomas/android_app/API/SingletonAPI.java
@@ -7,8 +7,12 @@ import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.Volley;
 
 public class SingletonAPI {
-
-    public static String BASE_URL = "http://192.168.0.105:8080/api/";
+    /**
+     * To use in the Android Studio emulator use: http://10.0.2.2:8080/api/
+     * To use in the Genymotion emulator use: http://10.0.2.3:8080/api/
+     * To use in a real device, get your pc IP: http://<local-pc-ip>:8080/api/
+     */
+    public static String BASE_URL = "http://10.0.2.2:8080/api/";
     private static SingletonAPI mSingletonAPIInstance;
     private RequestQueue mRequestQueue;
     private static Context mContext;
